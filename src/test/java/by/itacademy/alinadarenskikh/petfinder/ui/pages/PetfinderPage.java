7fcd1eec16d852bfb6206eb3b655185fd2472e77
AlinaDarenskikh <dawmond@mail.ru>
2023-04-04 23:05:32 +0300

    implemented  fillLogIn() into PrtfinderPage

--- src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/pages/PetfinderPage.java
+++ src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/pages/PetfinderPage.java
@@ -1,6 +1,14 @@
 package by.itacademy.alinadarenskikh.petfinder.ui.pages;
 
+import by.itacademy.alinadarenskikh.petfinder.ui.driver.Driver;
+import by.itacademy.alinadarenskikh.petfinder.ui.util.Util;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class PetfinderPage {
+
+    public static WebDriver driver = Driver.getDriver();
 
     public static final String URL="https://www.petfinder.com";
     public static final String FOOTER_TEXT = "©2023 Petfinder.com";
@@ -12,4 +20,16 @@ public class PetfinderPage {
     public static final String LOGIN_ERROR_MESSAGE_TEXT = "There was an error logging you into your account. Please try again. If you continue to have trouble logging in, your account may be locked. Your account is locked for 30 minutes after 5 incorrect login attempts. Recover your password.";
     public static final String PASSWORD_ERROR_MESSAGE_TEXT = "There was an error logging you into your account. Please try again. If you continue to have trouble logging in, your account may be locked. Your account is locked for 30 minutes after 5 incorrect login attempts. Recover your password.";
 
+
+    public static void fillLoginForm(String email, String password) {
+        Util.waitForElementLocatedBy(driver, By.xpath(PetfinderPage.LOGIN_FORM)).click();
+
+        WebElement emailField = driver.findElement(By.id("Login_Page_username"));
+        WebElement passwordField = driver.findElement(By.id("Login_Page_password"));
+
+        emailField.sendKeys(email);
+        passwordField.sendKeys(password);
+
+        driver.findElement(By.id("Login_Page_submit")).click();
+    }
 }
