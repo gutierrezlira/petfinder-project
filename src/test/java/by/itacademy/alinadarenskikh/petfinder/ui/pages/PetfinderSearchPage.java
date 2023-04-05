6f5b02d8c93150c32f4792271ff5d0174d99b018
AlinaDarenskikh <dawmond@mail.ru>
2023-04-05 12:19:48 +0300

    modify PetfinderStep.java

--- src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/pages/PetfinderSearchPage.java
+++ src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/pages/PetfinderSearchPage.java
@@ -1,11 +1,31 @@
 package by.itacademy.alinadarenskikh.petfinder.ui.pages;
 
+import by.itacademy.alinadarenskikh.petfinder.ui.driver.Driver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class PetfinderSearchPage {
+    static WebDriver driver;
+
+    public PetfinderSearchPage(WebDriver driver) {
+        this.driver = driver;
+        driver.get("https://www.petfinder.com/");
+    }
+
     final static public By  SEARCH_ANIMAL_TYPE = By.id("simpleSearchAnimalType");
     final static public By SEARCH_LOCATION = By.id("simpleSearchLocation");
     final static public By SEARCH_BUTTON = By.className("iconSearch");
     final static public String LOCATION_KEY = "New York";
-    final static public String TYPE_OF_ANIMAL_KEY = "cat";
+    final static public String TYPE_OF_ANIMAL = "cat";
+    final static public String CURRENT_LOCATION = "//*[@data-test ='Suggester_Current_Label']";
+
+    public static void sendKeys(String animalType, String location) {
+        driver.findElement(SEARCH_ANIMAL_TYPE).sendKeys(animalType);
+        driver.findElement(SEARCH_LOCATION).sendKeys(location);
+        driver.findElement(SEARCH_BUTTON).click();
+    }
+
 }
--- src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/tests/SearchTest.java
+++ src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/tests/SearchTest.java
@@ -2,6 +2,7 @@ package by.itacademy.alinadarenskikh.petfinder.ui.tests;
 
 import by.itacademy.alinadarenskikh.petfinder.ui.driver.Driver;
 import by.itacademy.alinadarenskikh.petfinder.ui.pages.PetfinderSearchPage;
+import by.itacademy.alinadarenskikh.petfinder.ui.steps.PetfinderSearchStep;
 import by.itacademy.alinadarenskikh.petfinder.ui.util.Util;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,34 +14,40 @@ import org.openqa.selenium.WebElement;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SearchTest {
     WebDriver driver = Driver.getDriver();
+    PetfinderSearchPage searchPage;
 
     @BeforeEach
     public void setUp() {
-
         driver.get("https://www.petfinder.com");
-       // petfinderStep = new PetfinderStep(driver);
+        searchPage = new PetfinderSearchPage(driver);
     }
 
     @Test
     public void searchResultNotEmpty(){
-   driver.findElement(PetfinderSearchPage.SEARCH_ANIMAL_TYPE).sendKeys("cat");
-   driver.findElement(PetfinderSearchPage.SEARCH_LOCATION).sendKeys("New York");
-   driver.findElement(PetfinderSearchPage.SEARCH_BUTTON).click();
+    PetfinderSearchPage.sendKeys(PetfinderSearchPage.TYPE_OF_ANIMAL, PetfinderSearchPage.LOCATION_KEY);
 
    List<WebElement> petCards = Collections.singletonList(Util.waitForElementLocatedBy(driver, By.className("petCard-body")));
    assertFalse(petCards.isEmpty(), "Search results are empty");
     }
 
     @Test
-    public void searchResultsContainCat() {
+    public void searchResultIsLocationIsCorrect() throws InterruptedException {
         driver.findElement(PetfinderSearchPage.SEARCH_ANIMAL_TYPE).sendKeys("cat");
         driver.findElement(PetfinderSearchPage.SEARCH_LOCATION).sendKeys("New York");
         driver.findElement(PetfinderSearchPage.SEARCH_BUTTON).click();
+
+        String actualLocation = Util.waitForElementLocatedBy(driver, By.xpath(PetfinderSearchPage.CURRENT_LOCATION)).getText();
+  Thread.sleep(5000);
+        assertEquals(PetfinderSearchPage.LOCATION_KEY, actualLocation);
+    }
+
+    @Test
+    public void searchResultsContainCat() {
+        PetfinderSearchPage.sendKeys(PetfinderSearchPage.TYPE_OF_ANIMAL, PetfinderSearchPage.LOCATION_KEY);
 
         List<WebElement> petCards = driver.findElements(By.cssSelector(".petCard-body-details-hdg .u-isVisuallyHidden"));
 
