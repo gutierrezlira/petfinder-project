103e848a5600ff6809c9fec6516e19cfe4aa0540
AlinaDarenskikh <dawmond@mail.ru>
2023-04-06 12:08:09 +0300

    apply chain-pattern

--- src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/pages/PetfinderPage.java
+++ src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/pages/PetfinderPage.java
@@ -3,39 +3,69 @@ package by.itacademy.alinadarenskikh.petfinder.ui.pages;
 import by.itacademy.alinadarenskikh.petfinder.ui.driver.Driver;
 import by.itacademy.alinadarenskikh.petfinder.ui.util.Util;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
 
 public class PetfinderPage {
 
     public static WebDriver driver = Driver.getDriver();
-
-    public static final String URL="https://www.petfinder.com";
-    public static final String FOOTER_TEXT = "©2023 Petfinder.com";
-    public static final String LOGIN_FORM = "//*[@id='react_menu-profile']/ul/li[2]/a";
-    //"//div[@class='mobileMenu-footer']//a[contains(text(),'Log In')]"; #react_menu-profile  a[href$="/"]
-    public static final String PROFILE_VERIFICATION_BUTTON = "//button[@class='header-inner-profile-accountBtn']";
-    public static final String FOOTER_XPATH = "//*[contains(text(),'©2023 Petfinder')]";
-    public static final String LOGIN_AND_PASSWORD_ERROR_MESSAGE ="//div[@data-test='Login_Error_Message']";
-    public static final String LOGIN_ERROR_MESSAGE_TEXT = "There was an error logging you into your account. Please try again. If you continue to have trouble logging in, your account may be locked. Your account is locked for 30 minutes after 5 incorrect login attempts. Recover your password.";
-    public static final String PASSWORD_ERROR_MESSAGE_TEXT = "There was an error logging you into your account. Please try again. If you continue to have trouble logging in, your account may be locked. Your account is locked for 30 minutes after 5 incorrect login attempts. Recover your password.";
-
-
-    public static void fillLoginForm(String email, String password) {
-        Util.waitForElementLocatedBy(driver, By.xpath(PetfinderPage.LOGIN_FORM)).click();
-
-        WebElement emailField = driver.findElement(By.id("Login_Page_username"));
-        WebElement passwordField = driver.findElement(By.id("Login_Page_password"));
-
-        emailField.sendKeys(email);
-        passwordField.sendKeys(password);
-
-        driver.findElement(By.id("Login_Page_submit")).click();
+    public PetfinderPage(WebDriver driver){
+        this.driver = driver;
     }
 
-    public static String getConfirmationData(WebDriver driver, By by) {
-        String actualUserName = Util.waitForElementLocatedBy(driver, by).getText();
-        return actualUserName;
+
+    public  final String URL="https://www.petfinder.com";
+    public final String FOOTER_TEXT = "©2023 Petfinder.com";
+    public  final String LOGIN_FORM = "//*[@id='react_menu-profile']/ul/li[2]/a";
+    //"//div[@class='mobileMenu-footer']//a[contains(text(),'Log In')]"; #react_menu-profile  a[href$="/"]
+    public  final String PROFILE_VERIFICATION_BUTTON = "//button[@class='header-inner-profile-accountBtn']";
+    public final String FOOTER_XPATH = "//*[contains(text(),'©2023 Petfinder')]";
+    public  final String LOGIN_AND_PASSWORD_ERROR_MESSAGE ="//div[@data-test='Login_Error_Message']";
+    public  final String LOGIN_ERROR_MESSAGE_TEXT = "There was an error logging you into your account. Please try again. If you continue to have trouble logging in, your account may be locked. Your account is locked for 30 minutes after 5 incorrect login attempts. Recover your password.";
+    public  final String PASSWORD_ERROR_MESSAGE_TEXT = "There was an error logging you into your account. Please try again. If you continue to have trouble logging in, your account may be locked. Your account is locked for 30 minutes after 5 incorrect login attempts. Recover your password.";
+
+
+    public  String getFooterText(){
+
+        return driver.findElement(By.xpath(FOOTER_XPATH)).getText();
+    }
+    public  PetfinderPage fillLoginForm(String email, String password) {
+        Util.waitForElementLocatedBy(driver, By.xpath(LOGIN_FORM)).click();
+
+                driver.findElement(By.id("Login_Page_username")).sendKeys(email);
+                driver.findElement(By.id("Login_Page_password")).sendKeys(password);
+    return this;
+    }
+
+    public PetfinderPage clickSubmitLogInForm(){
+        driver.findElement(By.id("Login_Page_submit")).click();
+        return this;
+    }
+
+    public String getActualLogInProfileName() {
+       String actualName = Util.waitForElementLocatedBy(driver, By.xpath(PROFILE_VERIFICATION_BUTTON)).getText();
+        return actualName;
+    }
+
+    public String getErrorMassage(){
+        String errorEntranceText = Util.waitForElementLocatedBy(driver, By.xpath(LOGIN_AND_PASSWORD_ERROR_MESSAGE)).getText();
+        return errorEntranceText;
+    }
+
+    public String fillEmptyFields(){
+
+        fillLoginForm("", "");
+
+        WebElement inputElement = driver.findElement(By.id("Login_Page_username"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        boolean isRequired = (Boolean)js.executeScript("return arguments[0].required;", inputElement);
+        if(isRequired)
+        {
+            return "Element is required";
+        }
+        return "Fields are filled";
     }
 
 }
--- src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/tests/LoginFormTest.java
+++ src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/tests/LoginFormTest.java
@@ -5,51 +5,62 @@ import org.openqa.selenium.WebDriver;
 
 import by.itacademy.alinadarenskikh.petfinder.ui.driver.Driver;
 import by.itacademy.alinadarenskikh.petfinder.ui.pages.PetfinderPage;
-import by.itacademy.alinadarenskikh.petfinder.ui.steps.PetfinderStep;
 import by.itacademy.alinadarenskikh.petfinder.ui.util.UserData;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginFormTest {
     WebDriver  driver;
-    private PetfinderStep petfinderStep;
+    PetfinderPage petPage;
 
     @BeforeEach
     public void setUp() {
         driver = Driver.getDriver();
-        driver.get("https://www.petfinder.com");
-        petfinderStep = new PetfinderStep(driver);
+        petPage = new PetfinderPage(driver);
+
+        driver.get(petPage.URL);
     }
 
     @Test
     @DisplayName("Verify Petfinder is open")
     public void testPetfinderOpen(){
-       assertEquals(PetfinderPage.FOOTER_TEXT, petfinderStep.getFooterText());
+
+        assertEquals(petPage.FOOTER_TEXT, petPage.getFooterText());
     }
 
      @Test
      @DisplayName("Verify valid login and password")
     public void testValidLoginValidPassword() {
-        assertEquals(UserData.EXPECTED_USER_NAME, petfinderStep.checkValidLoginAndPassword());
+        petPage.fillLoginForm(UserData.VALID_LOGIN, UserData.VALID_PASSWORD)
+            .clickSubmitLogInForm()
+            .getActualLogInProfileName();
+        assertEquals(UserData.EXPECTED_USER_NAME, petPage.getActualLogInProfileName());
     }
 
     @Test
     @DisplayName("Verify invalid login")
     public void testInvalidLogin() {
-        assertEquals(PetfinderPage.LOGIN_ERROR_MESSAGE_TEXT, petfinderStep.invalidLogin());
+        petPage.fillLoginForm(UserData.INVALID_LOGIN, UserData.VALID_PASSWORD)
+        .clickSubmitLogInForm();
+
+        assertEquals(petPage.LOGIN_ERROR_MESSAGE_TEXT, petPage.getErrorMassage());
     }
 
     @Test
     @DisplayName("Verify invalid password")
     public void testInvalidPassword() {
-        assertEquals(PetfinderPage.PASSWORD_ERROR_MESSAGE_TEXT, petfinderStep.invalidPassword());
+        petPage.fillLoginForm(UserData.VALID_LOGIN, UserData.INVALID_PASSWORD)
+        .clickSubmitLogInForm();
+
+        assertEquals(petPage.PASSWORD_ERROR_MESSAGE_TEXT, petPage.getErrorMassage());
     }
 
   @Test
   @DisplayName("Verify empty fields")
   public void testEmptyFields() {
-      assertEquals("Element is required", petfinderStep.fillEmptyFields());
+      assertEquals("Element is required", petPage.fillEmptyFields());
   }
+
 
     @AfterEach
     public void tearDown() {
