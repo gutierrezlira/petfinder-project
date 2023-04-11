fb7948ec4fad2702ffc1fcc9fb12d3f08fef7ceb
AlinaDarenskikh <dawmond@mail.ru>
2023-04-11 20:25:28 +0300

    SearchWithFilterStep.java Done

--- src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/steps/SearchWithFilterStep.java
+++ src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/steps/SearchWithFilterStep.java
@@ -1,4 +1,33 @@
 package by.itacademy.alinadarenskikh.petfinder.ui.steps;
 
-public class SearchWithFilterStep {
+import by.itacademy.alinadarenskikh.petfinder.ui.driver.Driver;
+import by.itacademy.alinadarenskikh.petfinder.ui.pages.PetfinderPage;
+import by.itacademy.alinadarenskikh.petfinder.ui.pages.SearchWithFilterPage;
+import org.openqa.selenium.WebDriver;
+
+public class SearchWithFilterStep extends SearchWithFilterPage {
+
+    WebDriver driver;
+    SearchWithFilterPage searchWithFilterPage;
+
+    public SearchWithFilterStep(SearchWithFilterPage searchPage) {
+        //super(driver);
+        super(Driver.getDriver());
+        this.searchWithFilterPage = searchPage;
+    }
+
+    public SearchWithFilterStep searchForKittenWithLongHairAndFemaleGender() {
+        clickSearchIcon()
+                .clickCatsOption()
+                .selectAge()
+                .clickLongHairOption()
+                .clickGenderSelectButton()
+                .clickSomeCat();
+
+        return this;
+    }
+
+    public String getSearchResult() {
+        return getSearchResultText();
+    }
 }
