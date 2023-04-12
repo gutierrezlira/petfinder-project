f0d589fcec341ba51feb9650d480e771cfff043c
AlinaDarenskikh <dawmond@mail.ru>
2023-04-12 11:38:47 +0300

    made Tests class extend BaseTest

--- src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/tests/SearchWithFilterPageTest.java
+++ src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/tests/SearchWithFilterPageTest.java
@@ -1,38 +1,18 @@
 package by.itacademy.alinadarenskikh.petfinder.ui.tests;
 
-import by.itacademy.alinadarenskikh.petfinder.ui.driver.Driver;
 import by.itacademy.alinadarenskikh.petfinder.ui.pages.SearchWithFilterPage;
 import by.itacademy.alinadarenskikh.petfinder.ui.steps.SearchWithFilterStep;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class SearchWithFilterPageTest {
-    WebDriver driver;
-    SearchWithFilterStep searchStep;
-
-    @BeforeEach
-    public void setUp() {
-        driver = Driver.getDriver();
-        searchStep = new SearchWithFilterStep(new SearchWithFilterPage(driver));
-        driver.get(searchStep.URL);
-    }
+public class SearchWithFilterPageTest extends BaseTest {
+    SearchWithFilterStep searchStep =new SearchWithFilterStep(new SearchWithFilterPage(driver));
 
    @Test
     public void findPerfectPetMatchTest()  {
        searchStep.searchForKittenWithLongHairAndFemaleGender();
        assertTrue(searchStep.getSearchResultText().endsWith(searchStep.expectedText),
                "Search result does not end with expected text: " + searchStep.expectedText);
-    }
-
-    @AfterEach
-    public void tearDown(){
-        {
-            Driver.close();
-        }
     }
 }
