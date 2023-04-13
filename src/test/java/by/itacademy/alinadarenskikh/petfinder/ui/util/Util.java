35a2da4209d572a57f69d31f60466bd749ef0abd
AlinaDarenskikh <dawmond@mail.ru>
2023-04-13 12:08:02 +0300

    modify

--- src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/util/Util.java
+++ src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/util/Util.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 import java.util.List;
 
-public class Util {
+public class Util{
     public static WebElement waitForElementLocatedBy(WebDriver driver, By by) {
         return  new WebDriverWait(driver, Duration.ofSeconds(5))
                 .until(ExpectedConditions
