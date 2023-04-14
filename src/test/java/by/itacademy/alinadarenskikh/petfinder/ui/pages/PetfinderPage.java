59c04bcc71794eb91f2035d5661bf163b460528e
AlinaDarenskikh <dawmond@mail.ru>
2023-04-14 13:21:05 +0300

    comment deleted

--- src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/pages/PetfinderPage.java
+++ src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/pages/PetfinderPage.java
@@ -19,10 +19,10 @@ public class PetfinderPage {
     public  final String URL="https://www.petfinder.com";
     public final String FOOTER_TEXT = "©2023 Petfinder.com";
     private String LOGIN_FORM = "//*[@id='react_menu-profile']/ul/li[2]/a";
-    //"//div[@class='mobileMenu-footer']//a[contains(text(),'Log In')]"; #react_menu-profile  a[href$="/"]
     private String PROFILE_VERIFICATION_BUTTON = "//button[@class='header-inner-profile-accountBtn']";
     private String FOOTER_XPATH = "//*[contains(text(),'©2023 Petfinder')]";
     private String LOGIN_AND_PASSWORD_ERROR_MESSAGE ="//div[@data-test='Login_Error_Message']";
+
     public final String ERROR_MESSAGE_TEXT = "There was an error logging you into your account. Please try again. If you continue to have trouble logging in, your account may be locked. Your account is locked for 30 minutes after 5 incorrect login attempts. Recover your password.";
 
 
