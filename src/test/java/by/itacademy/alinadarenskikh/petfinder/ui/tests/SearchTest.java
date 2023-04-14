c4ce5776a3dafe89f95b9f3e7f3c9b71329d7edd
AlinaDarenskikh <dawmond@mail.ru>
2023-04-14 14:23:54 +0300

    just one test left

--- src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/tests/SearchTest.java
+++ src/test/java/by/itacademy/alinadarenskikh/petfinder/ui/tests/SearchTest.java
@@ -1,50 +1,22 @@
 package by.itacademy.alinadarenskikh.petfinder.ui.tests;
 
 import by.itacademy.alinadarenskikh.petfinder.ui.pages.PetfinderSearchPage;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SearchTest extends BaseTest {
     PetfinderSearchPage searchPage = new PetfinderSearchPage();
 
-   @Test
-   @DisplayName("Search result is not empty")
-    public void searchResultNotEmpty(){
-       searchPage
-               .sendKeys("cat", "New York");
-
-        assertFalse(searchPage.isSEarchResultEmpty().isEmpty(), "Search results are empty");
-    }
 
     @Test
-    @DisplayName("Search results contain cat")
-    public void searchResultsContainCat() {
-        searchPage
-                .sendKeys("cat", "New York");
+    @DisplayName("Search results contain dog")
 
-        assertTrue(searchPage.getListOfSearchResult(), "Search results contain a cat animal");
+    public void searchResultsContainDog() {
+        searchPage.sendKeys("Dog", "New York");
+        searchPage.closeSearchConfirmationForm();
+
+        boolean dogFound = searchPage.isAnimalTypeFound("Dog");
+        assertTrue(dogFound, "Search results contain a dog animal");
     }
-
- /*  @Test
- @DisplayName("Search result is in correct location"
-    public void searchResultIsLocationIsCorrect() throws InterruptedException {
-        searchPage
-                .sendKeys("", "Mexico");
-        WebElement actualLocation = driver.findElement(By.xpath(searchPage.CURRENT_LOCATION));
-
-        try {
-            Thread.sleep(3000); // ожидание 1 секунды
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        String actualLocationText = actualLocation.getText();
-
-        assertEquals(searchPage.LOCATION_KEY, actualLocationText);
-
-        assertEquals(searchPage.LOCATION_KEY, actualLocation);
-    }*/
-
 }
